e2d64a8dc8f15aae3a498ae9243c14ceaa357680
Tony Billings <tony@tonysoft.com>
2024-06-16 04:26:24 -0700

    Address compiler warnings

--- src/stats.cu
+++ src/stats.cu
@@ -4,23 +4,22 @@
 #include <device_launch_parameters.h>
 #include <iostream>
 #include <vector>
+#include <c++/13/cstdarg>
 
 using namespace std;
 
 __global__ void calculate_totals(const float* data, const size_t record_count, const size_t field_count, float* totals) {
-    extern __shared__ float sdata[];
+    extern __shared__ float sdata_totals[];
 
     const unsigned int global_idx_x = blockIdx.x * blockDim.x + threadIdx.x;
     const unsigned int global_idx_y = blockIdx.y * blockDim.y + threadIdx.y;
     const unsigned int local_idx = threadIdx.x;
 
-    sdata[threadIdx.x] = 0.0f;
+    sdata_totals[local_idx] = 0.0f;
     __syncthreads();
 
-    float val = FP_NAN;
     if (global_idx_x < record_count && global_idx_y < field_count) {
-        val = data[global_idx_x * field_count + global_idx_y];
-        sdata[local_idx] = val;
+        sdata_totals[local_idx] = data[global_idx_x * field_count + global_idx_y];
     } else {
         return;
     }
@@ -28,25 +27,25 @@ __global__ void calculate_totals(const float* data, const size_t record_count, c
 
     for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
         if (local_idx < s) {
-            sdata[local_idx] += sdata[local_idx + s];
+            sdata_totals[local_idx] += sdata_totals[local_idx + s];
         }
         __syncthreads();
     }
 
     if (local_idx == 0) {
-        atomicAdd(&totals[global_idx_y], sdata[0]);
+        atomicAdd(&totals[global_idx_y], sdata_totals[0]);
     }
 }
 
 __global__ void calculate_means_stddevs(const float* data, const size_t record_count, const size_t field_count,
     const float* totals, float* means, float* std_devs) {
-    extern __shared__ float sdata[];
+    extern __shared__ float sdata_stddev[];
 
     const unsigned int global_idx_x = blockIdx.x * blockDim.x + threadIdx.x;
     const unsigned int global_idx_y = blockIdx.y;
     const unsigned int local_idx = threadIdx.x;
 
-    sdata[local_idx] = 0.0f;
+    sdata_stddev[local_idx] = 0.0f;
     __syncthreads();
 
     float val = 0.0f;
@@ -62,34 +61,33 @@ __global__ void calculate_means_stddevs(const float* data, const size_t record_c
 
     if (global_idx_x < record_count && global_idx_y < field_count) {
         const float diff = val - means[global_idx_y];
-        sdata[local_idx] = diff * diff;
+        sdata_stddev[local_idx] = diff * diff;
     }
     __syncthreads();
 
     for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
         if (local_idx < s) {
-            sdata[local_idx] += sdata[local_idx + s];
+            sdata_stddev[local_idx] += sdata_stddev[local_idx + s];
         }
         __syncthreads();
     }
 
     if (local_idx == 0) {
-        atomicAdd(&std_devs[global_idx_y], sdata[0]);
+        atomicAdd(&std_devs[global_idx_y], sdata_stddev[0]);
     }
 }
 
-static void cleanup(float* data, float* totals, float* means, float* std_devs) {
-    if (data != nullptr)
-        cudaFree(data);
+static void cleanup(int count, ...) {
+    va_list args;
+    va_start(args, count);
 
-    if (totals != nullptr)
-        cudaFree(totals);
+    for (int i = 0; i < count; i++) {
+        if (float* device_array = va_arg(args, float*); device_array != nullptr) {
+            cudaFree(device_array);
+        }
+    }
 
-    if (means != nullptr)
-        cudaFree(means);
-
-    if (std_devs != nullptr)
-        cudaFree(std_devs);
+    va_end(args);
 }
 
 // TODO: cleanup
@@ -102,39 +100,41 @@ bool calculate_stats(const vector<char>& data, const size_t field_count, const s
     }
     if (cudaMemcpy(d_data, data.data(), data.size(), cudaMemcpyHostToDevice) != cudaSuccess) {
         cerr << "Error: cudaMemcpy failed for d_data" << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(1, d_data);
         return false;
     }
     if (cudaMalloc(&d_totals, field_count * sizeof(float)) != cudaSuccess) {
         cerr << "Error: cudaMalloc failed for d_totals" << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(2, d_data, d_totals);
         return false;
     }
     if (cudaMalloc(&d_means, field_count * sizeof(float)) != cudaSuccess) {
         cerr << "Error: cudaMalloc failed for d_means" << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(3, d_data, d_totals, d_means);
         return false;
     }
     if (cudaMalloc(&d_std_devs, field_count * sizeof(float)) != cudaSuccess) {
         cerr << "Error: cudaMalloc failed for d_std_devs" << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
     cudaError_t err = cudaMemset(d_totals, 0, field_count * sizeof(float));
     if (err != cudaSuccess) {
         cerr << "Error: cudaMemset failed for d_totals: " << cudaGetErrorString(err) << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
     err = cudaMemset(d_means, 0, field_count * sizeof(float));
     if (err != cudaSuccess) {
         cerr << "Error: cudaMemset failed for d_means: " << cudaGetErrorString(err) << endl;
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
     err = cudaMemset(d_std_devs, 0, field_count * sizeof(float));
     if (err != cudaSuccess) {
         cerr << "Error: cudaMemset failed for d_std_devs: " << cudaGetErrorString(err) << endl;
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
@@ -149,14 +149,14 @@ bool calculate_stats(const vector<char>& data, const size_t field_count, const s
     err = cudaGetLastError();
     if (err != cudaSuccess) {
         cerr << "Error: calculate_totals failed: " << cudaGetErrorString(err) << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
     if (cudaDeviceSynchronize() != cudaSuccess) {
         err = cudaGetLastError();
         cerr << "Error: cudaDeviceSynchronize failed: " << cudaGetErrorString(err) << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
@@ -164,42 +164,42 @@ bool calculate_stats(const vector<char>& data, const size_t field_count, const s
     err = cudaGetLastError();
     if (err != cudaSuccess) {
         cerr << "Error: calculate_means_stddevs failed: " << cudaGetErrorString(err) << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
     if (cudaDeviceSynchronize() != cudaSuccess) {
         err = cudaGetLastError();
         cerr << "Error: cudaDeviceSynchronize failed: " << cudaGetErrorString(err) << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
     float h_totals[field_count];
     if (cudaMemcpy(h_totals, d_totals, field_count * sizeof(float), cudaMemcpyDeviceToHost) != cudaSuccess) {
         cerr << "Error: cudaMemcpy failed for h_totals" << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
     float h_means[field_count];
     if (cudaMemcpy(h_means, d_means, field_count * sizeof(float), cudaMemcpyDeviceToHost) != cudaSuccess) {
         cerr << "Error: cudaMemcpy failed for h_means" << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
     float h_std_devs[field_count];
     if (cudaMemcpy(h_std_devs, d_std_devs, field_count * sizeof(float), cudaMemcpyDeviceToHost) != cudaSuccess) {
         cerr << "Error: cudaMemcpy failed for h_std_devs" << endl;
-        cleanup(d_data, d_totals, d_means, d_std_devs);
+        cleanup(4, d_data, d_totals, d_means, d_std_devs);
         return false;
     }
 
     for (size_t i = 0; i < field_count; i++)
         h_std_devs[i] = sqrt(h_std_devs[i] / static_cast<float>(record_count));
 
-    cleanup(d_data, d_totals, d_means, d_std_devs);
+    cleanup(4, d_data, d_totals, d_means, d_std_devs);
 
     stats.totals.insert(stats.totals.end(), h_totals, h_totals + field_count);
     stats.means.insert(stats.means.end(), h_means, h_means + field_count);
